5e0fba787c347524b4fec1eb03db0f5cff678998
Truong-Nguyen <dangtruongnguyen2@gmail.com>
2024-10-23 17:09:54 +0700

    update

--- src/Components/Pages/UserManagement.jsx
+++ src/Components/Pages/UserManagement.jsx
@@ -1,10 +1,13 @@
-import { Button, Input, notification, Space, Table, Tag, Typography } from 'antd';
+import { Button, Form, Input, Modal, notification, Space, Table, Tag, Typography } from 'antd';
 import { useEffect, useState } from 'react';
 import axios from 'axios'; // Import Axios
+import { PlusOutlined } from '@ant-design/icons';
+import { DoAddNewUser } from '../../apis/Users/user';
 
 const UserManagement = () => {
     const { Title } = Typography;
     const { Search } = Input;
+    const [form] = Form.useForm();
 
     // useState
     const [accounts, setAccounts] = useState([]);
@@ -12,6 +15,7 @@ const UserManagement = () => {
     const [filteredAccounts, setFilteredAccounts] = useState([]); // State for filtered data
     const [currentPage, setCurrentPage] = useState(1);
 
+    const [isModalVisible, setIsModalVisible] = useState(false);
 
 
     // useEffect to load data from API on mount
@@ -47,6 +51,35 @@ const UserManagement = () => {
             setFilteredAccounts(filtered);
         }
     };
+
+    // Handle Create New User
+    const onFinish = async (values) => {
+        console.log("Form submit:", values);
+
+
+        try {
+            const { fullName, phoneNumber, email, password } = values;
+
+            const DoCreateUser = await DoAddNewUser(fullName, phoneNumber, email, password)
+            console.log('====================================');
+            console.log("DoCreateUser", DoCreateUser);
+            console.log('====================================');
+            if (DoCreateUser.status === 201) {
+                notification.success({
+                    message: 'Create New User Successfully',
+                    duration: 2
+                })
+                window.location.reload();
+            }
+        } catch (error) {
+            console.log(error);
+            notification.error({
+                message: 'Create New User Failed',
+                duration: 2
+            })
+        }
+    };
+
 
     // Delete An Account (This assumes you have a delete endpoint)
     const deleteAccount = async (id) => {
@@ -142,6 +175,10 @@ const UserManagement = () => {
         },
     ];
 
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+
     return (
         <>
             {/* Header */}
@@ -159,6 +196,15 @@ const UserManagement = () => {
                     style={{ margin: '20px 20px 20px 0', width: '33%' }}
                     onSearch={onSearch} // Call onSearch when user submits
                 />
+                <Button
+                    icon={<PlusOutlined />}
+                    size={'large'}
+                    type="primary"
+                    style={{ width: 'fit-content', margin: '20px', backgroundColor: '#1677FF' }}
+                    onClick={showModal}
+                >
+                    Create Account
+                </Button>
             </div>
 
             {/* Table */}
@@ -171,6 +217,63 @@ const UserManagement = () => {
                     onChange: onPageChange,
                 }}
             />
+
+            {/* Modal Create New User */}
+            <Modal
+                title="Create New User"
+                visible={isModalVisible}
+                okText="Submit"
+                cancelText="Cancel"
+                footer={[]}
+            >
+
+                <Form
+                    onFinish={onFinish}
+                    form={form}
+                    layout="vertical"
+                    name="create_user_form"
+                    initialValues={{ remember: true }}
+                >
+                    <Form.Item
+                        name="fullName"
+                        label="Full Name"
+                        rules={[{ required: true, message: 'Please input your full name!' }]}
+                    >
+                        <Input placeholder="Enter full name" />
+                    </Form.Item>
+                    <Form.Item
+                        name="phoneNumber"
+                        label="Phone Number"
+                        rules={[{ required: true, message: 'Please input your phone number!' }]}
+                    >
+                        <Input placeholder="Enter phone number" />
+                    </Form.Item>
+                    <Form.Item
+                        name="email"
+                        label="Email"
+                        rules={[{ required: true, message: 'Please input a valid email!', type: 'email' }]}
+                    >
+                        <Input placeholder="Enter email" />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        label="Password"
+                        rules={[{ required: true, message: 'Please input your password!' }]}
+                    >
+                        <Input.Password placeholder="Enter password" />
+                    </Form.Item>
+                    <Form.Item
+                        wrapperCol={{
+                            offset: 8,
+                            span: 16,
+                        }}
+                    >
+                        <Button type="primary" htmlType="submit">
+                            Submit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </>
     );
 };
--- src/apis/Users/user.jsx
+++ src/apis/Users/user.jsx
@@ -4,3 +4,12 @@ import axios from '../../utils/axios-custom';
 export const GetAllUsers = () => {
     return axios.get('User');
 }
+
+export const DoAddNewUser = (fullName, phoneNumber, email, password) => {
+    return axios.post('User', {
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password
+    })
+}
