f2521eb5bff7f0899bd5a4f081db408c4454a423
Minh Khang <nmkhang2632002@gmail.com>
2024-11-12 18:11:21 +0700

    replace new url backend

--- src/utils/axios-custom.jsx
+++ src/utils/axios-custom.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://103.179.184.153:5279/api/";
+const API_URL = "https://edy-learning.id.vn/api/";
 //// const API_URL = "https://localhost:7222/api/";
 // const API_URL = "http://localhost:5279/api/";
 const api = axios.create({
